cf28c038e7f92fce69b1de410c7a7f068bd7c51c
Lukas Hettwer <lukas.hettwer@gmail.com>
2017-01-29 18:26:52 +0100

    bugfix #37

--- src/base/util/Spielfeld.java
+++ src/base/util/Spielfeld.java
@@ -2,25 +2,23 @@ package util;
 
 import greenfoot.*;
 import interfaces.GreenfootWorld;
-import util.DialogUtils;
 
 import javax.swing.*;
 import java.util.*;
 import java.lang.*;
-import util.WeltSetup;
 
 /**
  * Spielfeldklasse kapselt die Funktionalität des Spielfeldes von der Logik der Welt.
  */
 public class Spielfeld {
 
-	private GreenfootWorld welt;
+    private GreenfootWorld welt;
     private WeltSetup setup;
 
-	public Spielfeld(GreenfootWorld welt, WeltSetup setup) {
-		this.welt = welt;
+    public Spielfeld(GreenfootWorld welt, WeltSetup setup) {
+        this.welt = welt;
         this.setup = setup;
-	}
+    }
 
     public GreenfootWorld getWelt() {
         return welt;
@@ -28,13 +26,13 @@ public class Spielfeld {
 
     public void objektHinzufuegen(Actor object, int x, int y) {
         //TODO
-        if(x >= setup.getWidth())
-            x = setup.getWidth()-1;
+        if (x >= setup.getWidth())
+            x = setup.getWidth() - 1;
 
-        if(y >= setup.getHeight() )
-            y = setup.getHeight()-1;
+        if (y >= setup.getHeight())
+            y = setup.getHeight() - 1;
 
-        if(x < 0 || y < 0) {
+        if (x < 0 || y < 0) {
             String message = "<html>" +
                     "<p>Die Koordinaten müssen einen Wert größer oder gleich 0 besitzen.</p>" +
                     "</html>";
@@ -48,12 +46,19 @@ public class Spielfeld {
     }
 
 
-    public void objektEntfernen(Actor object){
+    public void objektEntfernen(Actor object) {
         welt.removeObject(object);
     }
 
     public List<Actor> gibObjekteAuf(int x, int y, Class cls) {
-    	return welt.getObjectsAt(setup.getOffsetStartToX() + x, setup.getOffsetStartToY() + y, cls);
+        return welt.getObjectsAt(setup.getOffsetStartToX() + x, setup.getOffsetStartToY() + y, cls);
+    }
+
+    public Actor gibObjektAuf(int x, int y, Class cls) {
+        List<Actor> foo = welt.getObjectsAt(setup.getOffsetStartToX() + x, setup.getOffsetStartToY() + y, cls);
+        if (foo.size() > 0)
+            return foo.get(0);
+        return null;
     }
 
 }
