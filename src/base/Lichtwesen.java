cb7e5694451e52fb90ff61bfdb2d652a88e46cde
Lukas Hettwer <lukas.hettwer@gmail.com>
2017-01-29 17:49:17 +0100

    Lichtwesen jetzt auf deutsch

--- src/base/Lichtwesen.java
+++ src/base/Lichtwesen.java
@@ -8,61 +8,92 @@ import java.util.ArrayList;
 
 class Lichtwesen extends Figur implements LichtwesenInterface {
     private LichtwesenFarbe farbe;
+    private ArrayList<GreenfootImage> bewegung;
 
     public Lichtwesen() {
         farbe = LichtwesenFarbe.BLAU;
+        bewegung = loadImageForAnimation(new GreenfootImage("lichtwesen_beschwören.png"), 6);
         aktualisiereBild();
     }
 
-    private ArrayList<GreenfootImage> getAnimationContainer() {
-        return loadImageForAnimation(new GreenfootImage("lichtwesen_beschwören.png"), 6);
-    }
+    /**
+     * Das Lichtwesen bewegt sich auf der Stelle.
+     */
 
-    public void makeAnimation() {
-        for (GreenfootImage img : getAnimationContainer()) {
+    public void bewegen() {
+        for (GreenfootImage img : bewegung) {
             setImage(img);
             Greenfoot.delay(2);
         }
         aktualisiereBild();
     }
 
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Beige Cappuchino.
+     */
+
     public void leuchteBeigeCappuchino() {
         farbe = LichtwesenFarbe.BEIGE;
         aktualisiereBild();
     }
 
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Gelb.
+     */
+
     public void leuchteGelb() {
         farbe = LichtwesenFarbe.GELB;
         aktualisiereBild();
-
     }
+
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Lila.
+     */
 
     public void leuteLila() {
         farbe = LichtwesenFarbe.LILA;
         aktualisiereBild();
     }
 
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Rosa.
+     */
+
     public void leuteRosa() {
         farbe = LichtwesenFarbe.ROSA;
         aktualisiereBild();
     }
 
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Rot.
+     */
+
     public void leuchteRot() {
         farbe = LichtwesenFarbe.ROT;
         aktualisiereBild();
-
     }
+
+    /**
+     * Aendert das Aussehen des Leuchtwesen in Blau.
+     */
 
     public void leuchteBlau() {
         farbe = LichtwesenFarbe.BLAU;
         aktualisiereBild();
-
     }
+
+    /**
+     * Aendert die Farbe des Leuchtwesen.
+     */
 
     public void wechselFarbe() {
         farbe = farbe.next();
         aktualisiereBild();
     }
+
+    /**
+     * Aktualisiert das Bild des Lichtwesen.
+     */
 
     private void aktualisiereBild() {
         setImage(farbe.bild());
